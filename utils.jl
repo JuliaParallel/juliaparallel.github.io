44a80174d330ae1e1d70afc5ed4470750f46f704
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2022-05-09 17:01:26 -0400

    New template, first version.

--- utils.jl
+++ utils.jl
@@ -1,3 +1,5 @@
+using DelimitedFiles
+
 function hfun_bar(vname)
   val = Meta.parse(vname[1])
   return round(sqrt(val), digits=2)
@@ -21,9 +23,30 @@ end
 Plug in the list of news contained in the `/news/` folder.
 """
 function hfun_news()
-    curyear = year(Dates.today())
+    # Author badges
+    author_badges_mat = readdlm("_assets/author-badges.dat", ':')
+    author_badges = Dict()
+    for i = 1:size(author_badges_mat,1)
+       author_badges[author_badges_mat[i]] =
+        rstrip.(lstrip.(split(author_badges_mat[i,2], ",")))
+    end
+    
+    # Changing page width in "News" section 
     io = IOBuffer()
-    for year in curyear:-1:2021
+    write(io, """<style>
+                    @media (min-width: 1024px) {
+                      .franklin-content {
+                         width: 100%;
+                         padding-left: 0%;
+                         padding-right: 0%;
+                      }
+                    }
+                 </style>""")
+    
+    # Add news
+    curyear = year(Dates.today())
+    write(io, """<div class="news-container">""")
+    for year in curyear:-1:2022
         ys = "$year"
         year < curyear && write(io, "\n**$year**\n")
         for month in 12:-1:1
@@ -38,7 +61,9 @@ function hfun_news()
                 url = "/news/$ys/$ms/$ps/"
                 surl = strip(url, '/')
                 title = pagevar(surl, :title)
-                title === nothing && (title = "Untitled")
+                short_descr = pagevar(surl, :short_descr)
+                author = pagevar(surl, :author)
+                width = rand(250:290)
                 pubdate = pagevar(surl, :published)
                 if isnothing(pubdate)
                     date    = "$ys-$ms-01"
@@ -47,16 +72,23 @@ function hfun_news()
                     date    = Date(pubdate, dateformat"d U Y")
                     days[i] = day(date)
                 end
-                lines[i] = "\n[$title]($url) $date \n"
+                lines[i] = """""" 
+                
+                lines[i] *= """<div class="news-block" style="width:$(width)px;margin:-5px;">"""
+                lines[i] *= """<a href="$url">$title</a> $date<br>"""
+                lines[i] *= """<p>$short_descr</p>"""
+                lines[i] *= """$author"""
+                for b in author_badges[author]
+                      lines[i] *= """<img src="/assets/$b.png"/>"""
+                end
+                lines[i] *= """</div>"""
             end
             # sort by day
             foreach(line -> write(io, line), lines[sortperm(days, rev=true)])
         end
     end
-    # markdown conversion adds `<p>` beginning and end but
-    # we want to  avoid this to avoid an empty separator
-    r = Franklin.fd2html(String(take!(io)), internal=true)
-    return r
+    write(io, """</div>""")
+    return String(take!(io))
 end
 
 """
@@ -66,7 +98,7 @@ Input the 3 latest news posts.
 """
 function hfun_recentnews()
     curyear = Dates.Year(Dates.today()).value
-    ntofind = 3
+    ntofind = 12
     nfound  = 0
     recent  = Vector{Pair{String,Date}}(undef, ntofind)
     for year in curyear:-1:2019
@@ -104,12 +136,25 @@ function hfun_recentnews()
         title === nothing && (title = "Untitled")
         sdate = "$(day(date)) $(monthname(date)) $(year(date))"
         blurb = pagevar(surl, :rss)
+        short_descr = pagevar(surl, :short_descr)
+        width = pagevar(surl, :width)
+        #write(io, """
+        #    <div class="col-lg-4 col-md-12 blog">
+        #      <h3><a href="$url" class="title" data-proofer-ignore>$title</a>
+        #      </h3><span class="article-date">$date</span>
+        #      <p>$blurb</p>
+        #    </div>
+        #    """)
         write(io, """
-            <div class="col-lg-4 col-md-12 blog">
-              <h3><a href="$url" class="title" data-proofer-ignore>$title</a>
-              </h3><span class="article-date">$date</span>
-              <p>$blurb</p>
-            </div>
+            <a href="$url">
+              <div style="width:$(width)px;font-family: nyt-cheltenham,georgia,'times new roman',times,serif;color:#5A5A5A;"">
+              <h4 >
+              $title</h3>
+              <span style="font-size:15px;">$date</span>
+              <p style="font-family: nyt-cheltenham,georgia,'times new roman',times,serif;font-size:15px;color:#5A5A5A;">
+              $short_descr</p>
+              </div>
+            </a>
             """)
     end
     return String(take!(io))
